de4227c15c44fe2017181cd908990a3fba70a32c
Koussaila Messai <kmessai@et.esiea.fr>
2026-01-18 16:52:25 +0100

    Fix: Supprimer BOM du fichier SwaggerConfig.java

--- src/main/java/com/bibliotheque/gestion_bibliotheque/config/SwaggerConfig.java
+++ src/main/java/com/bibliotheque/gestion_bibliotheque/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-﻿package com.bibliotheque.gestion_bibliotheque.config;
+package com.bibliotheque.gestion_bibliotheque.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
